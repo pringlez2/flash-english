b5bb791fce04e5da79a19e0379da45e29ff2923c
pringlez2 <pringlez2@gmail.comm>
2026-02-26 17:53:44 +0900

    Make home page dynamic to avoid DB access during build

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import { getStudyQueue } from "@/lib/study-queue";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const [todayQueue, recentCards] = await Promise.all([
     getStudyQueue(20, 10),
